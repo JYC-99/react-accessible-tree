f392756488836b57d5c80f17209ae2a2a06f9bcb
Yucong Jin <yucongj@microsoft.com>
2020-02-24 11:53:09 +0800

    update demo

--- stories/index.tsx
+++ stories/index.tsx
@@ -33,10 +33,136 @@ storiesOf("ReactAccessibleTree", module).add("Default", () => {
               id: "McIntosh",
               title: (
                 <a href="https://en.wikipedia.org/wiki/McIntosh_%28apple%29">
-                  McIntosh{" "}
+                  McIntosh
                 </a>
               ),
               searchKeys: ["McIntosh"],
+              children: []
+            },
+            {
+              id: "Granny Smith",
+              title: (
+                <a href="https://en.wikipedia.org/wiki/Granny_Smith">
+                  Granny Smith
+                </a>
+              ),
+              searchKeys: ["Granny", "Smith"],
+              children: []
+            },
+            {
+              id: "Fuji",
+              title: (
+                <a href="https://en.wikipedia.org/wiki/Fuji_(apple)">Fuji</a>
+              ),
+              searchKeys: ["Fuji"],
+              children: []
+            }
+          ]
+        },
+        {
+          title: <a href="https://en.wikipedia.org/wiki/Banana">Bananas</a>,
+          id: "bananas",
+          searchKeys: ["bananas"],
+          children: []
+        }
+      ]
+    },
+    {
+      id: "vegetables",
+      title: <h3>Vegetables</h3>,
+      searchKeys: ["vegetables"],
+      children: [
+        {
+          id: "Podded Vegetables",
+          title: "Podded Vegetables",
+          searchKeys: ["Podded Vegetables"],
+          children: [
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Lentil">Lentil</a>,
+              id: "lentil",
+              searchKeys: ["lentil"],
+              children: []
+            },
+            {
+              title: (
+                <a href="https://en.wikipedia.org/wiki/Pea" role="link">
+                  Pea
+                </a>
+              ),
+              id: "pea",
+              searchKeys: ["pea"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Peanut">Peanut</a>,
+              id: "peanut",
+              searchKeys: ["peanut"],
+              children: []
+            }
+          ]
+        },
+        {
+          id: "Bulb and Stem Vegetables",
+          title: "Bulb and Stem Vegetables",
+          searchKeys: ["Bulb and Stem Vegetables"],
+          children: [
+            {
+              title: (
+                <a href="https://en.wikipedia.org/wiki/Asparagus">Asparagus</a>
+              ),
+              id: "Asparagus",
+              searchKeys: ["Asparagus"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Celery">Celery</a>,
+              id: "Celeryea",
+              searchKeys: ["Celery"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Leek">Leek</a>,
+              id: "leek",
+              searchKeys: ["leek"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Onion">Onion</a>,
+              id: "Onion",
+              searchKeys: ["Onion"],
+              children: []
+            }
+          ]
+        },
+        {
+          id: "Root and Tuberous Vegetables",
+          title: "Root and Tuberous Vegetables",
+          searchKeys: ["Root and Tuberous Vegetables"],
+          children: [
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Carrot">Carrot</a>,
+              id: "Carrot",
+              searchKeys: ["Carrot"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Ginger">Ginger</a>,
+              id: "Ginger",
+              searchKeys: ["Ginger"],
+              children: []
+            },
+            {
+              title: (
+                <a href="https://en.wikipedia.org/wiki/Parsnip">Parsnip</a>
+              ),
+              id: "Parsnip",
+              searchKeys: ["Parsnip"],
+              children: []
+            },
+            {
+              title: <a href="https://en.wikipedia.org/wiki/Potato">Potato</a>,
+              id: "Potato",
+              searchKeys: ["Potato"],
               children: []
             }
           ]
